75675b71f84e23c7b604f9561fb7ed704451a672
Sreeraj-SR <sreerajsree9961@gmail.com>
2026-03-05 17:32:07 +0530

    minor changes for server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -61,12 +61,11 @@ app.use(
 );
 app.use(express.json({ limit: "2mb" }));
 
-app.use(cookieParser()); // 
+app.use(cookieParser());  
 
 app.get("/", (req, res) => {
   res.json({ message: "Event Space SaaS Backend Running" });
 });
-
 
 // Auth Routes
 app.use("/api/auth", authRoutes);
